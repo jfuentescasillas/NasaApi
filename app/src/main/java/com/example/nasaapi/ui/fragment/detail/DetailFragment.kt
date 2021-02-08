3697b244b91b549131b52bb91063e2b3a137d602
JorgeFuentesCasillas <j.fuentes.casillas@gmail.com>
2021-02-08 20:01:44 +0100

    F001 DataBinding

--- app/src/main/java/com/example/nasaapi/ui/fragment/detail/DetailFragment.kt
+++ app/src/main/java/com/example/nasaapi/ui/fragment/detail/DetailFragment.kt
@@ -5,15 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.nasaapi.R
+import com.example.nasaapi.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+    lateinit var binding: FragmentDetailBinding
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/nasaapi/ui/fragment/list/ListFragment.kt
+++ app/src/main/java/com/example/nasaapi/ui/fragment/list/ListFragment.kt
@@ -5,15 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.nasaapi.R
+import com.example.nasaapi.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return binding.root
     }
 }
