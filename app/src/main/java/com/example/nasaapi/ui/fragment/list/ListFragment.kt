e9531ebbdba2f3bbd6d10a1503b12f6b7337e5b6
JorgeFuentesCasillas <j.fuentes.casillas@gmail.com>
2021-02-10 13:40:46 +0100

    F001 Manage View State with one live data

--- app/src/main/java/com/example/nasaapi/ui/fragment/list/ListFragment.kt
+++ app/src/main/java/com/example/nasaapi/ui/fragment/list/ListFragment.kt
@@ -8,8 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.nasaapi.base.BaseState
 import com.example.nasaapi.databinding.FragmentListBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import retrofit2.HttpException
+import java.net.UnknownHostException
 
 class ListFragment : Fragment() {
     private lateinit var binding: FragmentListBinding
@@ -19,6 +22,40 @@ class ListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentListBinding.inflate(inflater, container, false)
+
+        // Observer
+        viewModel.getState().observe(viewLifecycleOwner, { state ->
+            when (state) {
+                is BaseState.Error -> {
+                    binding.fragmentListProgressBar.visibility = View.GONE
+                    adapter.updateList(listOf())
+
+                    val msg = when (state.dataError) {
+                        is HttpException -> "Fatal Error: " + state.dataError.code().toString()
+                        is UnknownHostException -> "No internet connection"
+                        else -> "Generic Error"
+                    }
+
+                    // Show error message
+                    MaterialAlertDialogBuilder(requireActivity())
+                            .setTitle("Error")
+                            .setMessage(msg)
+                            .setPositiveButton("Retry") { dialog, which ->
+                                // Respond to positive button press
+                                viewModel.requestInformation()
+                            }
+                            .show()
+                }
+                is BaseState.Loading -> {
+                    binding.fragmentListProgressBar.visibility = View.VISIBLE
+                }
+                is BaseState.Normal -> {
+                    binding.fragmentListProgressBar.visibility = View.GONE
+                    binding.fragmentListSwipeRefreshLayout.isRefreshing = false
+                    adapter.updateList((state.data as ListState).picturesList)
+                }
+            }
+        })
 
         // Set recycler view
         adapter = ListAdapter(listOf(), requireActivity())
@@ -34,28 +71,6 @@ class ListFragment : Fragment() {
             adapter.updateList(listOf())
             viewModel.requestInformation()
         }
-
-        // Observers
-        viewModel.getResponse().observe(viewLifecycleOwner, { response ->
-            binding.fragmentListSwipeRefreshLayout.isRefreshing = false
-            adapter.updateList(response)
-        })
-        viewModel.getError().observe(viewLifecycleOwner, { error ->
-            adapter.updateList(listOf())
-
-            // Show error message
-            MaterialAlertDialogBuilder(requireActivity())
-                    .setTitle("Error")
-                    .setMessage(error)
-                    .setPositiveButton("Retry") { dialog, which ->
-                        // Respond to positive button press
-                        viewModel.requestInformation()
-                    }
-                    .show()
-        })
-        viewModel.isLoading().observe(viewLifecycleOwner, { loading ->
-           binding.fragmentListProgressBar.visibility = if (loading) View.VISIBLE else View.GONE
-        })
 
         // Request information and return it
         viewModel.requestInformation()
--- app/src/main/java/com/example/nasaapi/ui/fragment/list/ListViewModel.kt
+++ app/src/main/java/com/example/nasaapi/ui/fragment/list/ListViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.nasaapi.base.BaseState
 import com.example.nasaapi.ui.data.NasaRepository
 import com.example.nasaapi.ui.data.model.Item
 import kotlinx.coroutines.Dispatchers
@@ -15,37 +16,19 @@ import java.net.UnknownHostException
 // La lógica debe estar siempre en el ViewModel ****
 
 class ListViewModel: ViewModel() {
-    // Create a LiveData with a String
-    private val response = MutableLiveData<List<Item>>()
-    fun getResponse(): LiveData<List<Item>> = response
-
-    private val error = MutableLiveData<String>()
-    fun getError(): LiveData<String> = error
-
-    private val loading = MutableLiveData<Boolean>()
-    fun isLoading(): LiveData<Boolean> = loading
+    private val state = MutableLiveData<BaseState>()
+    fun getState(): LiveData<BaseState> = state
 
 
     fun requestInformation() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                loading.postValue(true)
+                state.postValue(BaseState.Loading())
 
                 val items = NasaRepository().requestNasaPictures("sun")
-                response.postValue(items)
-
-                loading.postValue(false)
+                state.postValue(BaseState.Normal(ListState(items)))
             } catch(e: Exception) {
-                loading.postValue(false)
-
-                when (e) {
-                    is HttpException -> {
-                        error.postValue("Fatal Error: " + e.code().toString())
-                    }
-                    is UnknownHostException -> {
-                        error.postValue("No internet connection")
-                    }
-                }
+                state.postValue(BaseState.Error(e))
             }
         }
     }
